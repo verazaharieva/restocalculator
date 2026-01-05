fb0f8addfb0d5cb3668635c674e00116cd74e332
verazaharieva <vera.zaharieva@outlook.com>
2026-01-05 18:56:29 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,17 @@ import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(() => {
+        console.log("Service Worker registered");
+      })
+      .catch((error) => {
+        console.error("Service Worker registration failed:", error);
+      });
+  });
+}
+
